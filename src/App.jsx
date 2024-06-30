508a5651a076f1917d58f8ec7e3e97d132794ffb
atanu16 <atanumax2@gmail.com>
2024-06-30 21:05:00 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/github.png'
-import viteLogo from './assets/smile.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/github.png";
+import viteLogo from "./assets/smile.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://github.com/atanu16" target="_blank">
+        <a href="https://atanu16.vercel.app/" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://github.com/atanu16" target="_blank">
@@ -22,14 +22,15 @@ function App() {
           count is {count}
         </button>
         <p>
-        The site you want to access is under development and has not been released yet.
+          The site you want to access is under development and has not been
+          released yet.
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Atanu&apos;s and github&apos;s logo to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
